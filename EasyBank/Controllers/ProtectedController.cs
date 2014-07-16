f21ef84a7cb6609e32ba7171ff97add54edee853
MVolodya <volodimir.medvid@bk.ru>
2014-07-16 17:51:25 +0300

    small fixes made
    show clientslist and references

--- EasyBank/Controllers/ProtectedController.cs
+++ EasyBank/Controllers/ProtectedController.cs
@@ -38,26 +38,57 @@ namespace EasyBank.Controllers
         [HttpGet]
         public ActionResult AddClient()
         {
-
             return View();
         }
         
         [HttpPost]
         public ActionResult AddClient(Client client)
         {
+            if (client != null && client.Name != null && client.PIdNumber != null && client.BirthDate != null && client.Email != null && client.RegistrationDate != null)
+            {
+                client.RegistrationDate = DateTime.Now;
+                db.Clients.Add(client);
+                db.SaveChanges();
+            }
+            else
+            {
+                ViewBag.Message = "Problem with inputted data";
+                RedirectToAction("/");
+            }
             return RedirectToAction("/");
         }
 
         [HttpGet]
-        public ActionResult EditClient()
+        public ActionResult EditClient(int? id)
         {
-            return View();
+            Client client=null;
+            if (id!=0)
+                client = db.Clients.FirstOrDefault(c=>c.ClientId==id);
+            if(client != null)
+            {
+                return View();     
+            }
+            else 
+            {
+                ViewBag.Message = "No client with this Id";
+                return RedirectToAction("/ListUsers");
+            }
         }
 
         [HttpPost]
-        public ActionResult EditClient(int? id)
+        public ActionResult EditClient(Client client)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "OOps.. Something wrong with data";
+                return RedirectToAction("/EditClient");
+            }
         }
 
         [HttpGet]
