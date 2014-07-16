a6f9153de03d0b0abc8bfcaea1e5d90db9b07858
Alexsus87 <a.nikonov.ums@gmail.com>
2014-07-16 15:50:44 +0300

    Added FileUpload

---
+++ EasyBank/Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyBank.Controllers
+{
+    public class FileController : Controller
+    {
+        //
+        // GET: /File/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                string pic = System.IO.Path.GetFileName(file.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/images/profile"), pic);
+
+               file.SaveAs(path);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    file.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+            }
+            return View();
+        }
+
+    }
+}
